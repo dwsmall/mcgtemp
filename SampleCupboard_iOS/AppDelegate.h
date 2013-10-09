64e267c896eb48e2b387ad585356ebaed3a2a624
D.W.Small <dwsmall@gmail.com>
2013-10-09 10:30:22 -0400

    Update to AppDelegate Basics

--- SampleCupboard_iOS/AppDelegate.h
+++ SampleCupboard_iOS/AppDelegate.h
@@ -12,6 +12,46 @@
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) UISplitViewController *splitViewController;
+
+
+// current view
+@property (strong, nonatomic) NSString *globalMode;
+
+// transition modes (user chose...)
+@property (strong, nonatomic) NSString *globalHcpChosen;
+@property (strong, nonatomic) NSString *globalProductChosen;
+
+// hcp dictionary
+@property (strong, nonatomic) NSDictionary *globalHcpDictionary;
+
+// retain values
+@property (strong, nonatomic) NSString *globalShipInfo;
+@property (strong, nonatomic) NSString *globalShipType;
+
+// screen handling
+@property (strong, retain) NSMutableArray *globalProductsRmv;
+@property (strong, retain) NSMutableArray *globalProductsScrn;
+
+@property (strong, nonatomic) NSString *globalRmvProductsFetch;
+
+
+// credentials on login
+@property (strong, retain) NSString *globalUserID;
+@property (strong, retain) NSString *globalToken;
+@property (strong, retain) NSString *globalBaseUrl;
+@property (strong, retain) NSString *globalClientId;
+@property (strong, retain) NSString *globalAllocationId;
+
+
+
+
+
+// not really needed
+@property (strong, nonatomic) NSManagedObject *globalOrderMO;
+@property (strong, nonatomic) NSManagedObject *globalHcpMO;
+
+
 
 // Data Management
 
@@ -22,6 +62,8 @@
 - (void)saveContext;
 - (NSURL *)applicationDocumentsDirectory;
 
-// Data Management 
+// Data Management
+
+
 
 @end
