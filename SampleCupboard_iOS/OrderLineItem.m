fb0039c80aa5109539f0365ff0c0da3db2e6848b
D.W.Small <dwsmall@gmail.com>
2013-10-15 11:40:46 -0400

    Code Clean Up

---
+++ SampleCupboard_iOS/OrderLineItem.m
@@ -0,0 +1,27 @@
+//
+//  OrderLineItem.m
+//  SampleCupboard_iOS
+//
+//  Created by David Small on 10/9/13.
+//  Copyright (c) 2013 MCG. All rights reserved.
+//
+
+#import "OrderLineItem.h"
+#import "Order.h"
+
+
+@implementation OrderLineItem
+
+@dynamic clientid;
+@dynamic datecreated;
+@dynamic datemodified;
+@dynamic id;
+@dynamic orderid;
+@dynamic productid;
+@dynamic quantityordered;
+@dynamic stored_product_code;
+@dynamic stored_product_description;
+@dynamic stored_product_name;
+@dynamic toOrderHeader;
+
+@end
