84c8d94fcfd6de1798678b3ccda0ed2736d1d1e0
D.W.Small <dwsmall@gmail.com>
2013-10-16 15:04:33 -0400

    Update to language localization files

--- SampleCupboard_iOS/HomeViewController.m
+++ SampleCupboard_iOS/HomeViewController.m
@@ -20,26 +20,84 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    // get language setting
+    
+    NSUserDefaults* defs = [NSUserDefaults standardUserDefaults];
+    NSArray* languages = [defs objectForKey:@"AppleLanguages"];
+    NSString* preferredLang = [languages objectAtIndex:0];
+    
+    
+    // set english
+    
+    NSString *hometext1 = @"Welcome to the IPAD sampling application HOME page that contains many useful links and resources to facilitate the IPAD sampling process:";
+    
+    NSString *hometext2 = @"Click here to Access the IPAD user manual";
+    
+    NSString *fileName = @"http://www.samplecupboard.com/content/ipad/MCG_Sampling_IPAD_EN.pdf";
+    
+    NSString *hometext3 = @"To contact us please use one of the links below:";
+    
+    NSString *hometext4 = @"Email MCG for questions regarding your samples or application functionality and training:";
+    
+    NSString *hometext5 = @"Email Canada, SFE for any questions relating to the IPAD device issues, suggestions or tips:";
+    
+    NSString *hometext6 = @"If HELP desk cannot help you with an application issue you may contact the MCG application support team:";
+    
+    NSString *hometext6a = @"Useful Websites:";
+    
+    NSString *hometext7 = @"Click here to access the ATS Healthcare tracking website to track the delivery of your sample requests:";
+    
+    NSString *hometext8 = @"Link To SampleCupboard:";
+    
+    
+    // french override
+    
+    if ([preferredLang isEqualToString:@"fr"]) {
+        
+        hometext1 = @"Bienvenue sur la page d'accueil de l'application MCG pour iPad oú vous y trouverez plusieurs liens utiles ainsi que des ressources pour faciliter le processus d'échantillonnage sur iPad:";
+        
+        hometext2 = @"Veuillez cliquer ici pour accéder le Guide de l'utilisateur";
+        
+        fileName = @"http://www.samplecupboard.com/content/ipad/MCG_Sampling_IPAD_FR.pdf";
+        
+        hometext3 = @"Pour nous contacter, veuillez utiliser l'un des liens ci-dessous:";
+        
+        hometext4 = @"courriel à MCG pour les questions concernant vos échantillons ou les fonctions de l'application et de la formation:";
+        
+        hometext5 = @"courriel à Canada, SFE pour toutes questions relatives aux problèmes de l'appareil IPAD ou pour soumettre vos suggestions ou des conseils:";
+        
+        hometext6 = @"Si le Bureau d'assistance ne peut vous seconder avec un problème d'application MCG, vous pouvez contacter l'équipe de support MCG:";
+        
+        hometext6a = @"Liens à certains sites Web utiles:";
+        
+        hometext7 = @"Cliquez ici pour accéder le site ATS Santé pour faire le suivi sur la livraison de vos demandes d'échantillons:";
+        
+        hometext8 = @"Lien à SampleCupboard:";
+        
+    }
+    
+    
+    
     // Build String Using Values From Localized Files
     NSArray  *array1 = [NSArray arrayWithObjects: @"<body style='background-color:white;color:#000'>",
                         @"<div>",
                         @"<span>",
-                        @"Welcome to the IPAD sampling application HOME page that contains many useful links and resources to facilitate the IPAD sampling process:",
+                        hometext1,
                         @"</span>",
                         
                         @"<p><ul><li>",
                         @"<a href='",
-                        @"http://www.samplecupboard.com/content/ipad/MCG_Sampling_IPAD_EN.pdf",
+                        fileName,
                         @"'>",
-                        @"Click here to Access the IPAD user manual",
+                        hometext2,
                         @"</a>",
                         @"</li></ul></p>",
                         
                         @"<span>",
-                        @"To contact us please use one of the links below:",
+                        hometext3,
                         @"</span>",
                         @"<p><ul><li>",
-                        @"Email MCG for questions regarding your samples or application functionality and training:",
+                        hometext4,
                         @"</li></ul></p>",
                         
                         @"<p><ul><li>",
@@ -50,8 +108,12 @@
                         @"</a> ",
                         @"</li></ul></p>",
                         
+                        @"<p><ul><li>",
+                        hometext5,
+                        @"</li></ul></p>",
+                        
                         @"<span>",
-                        @"If HELP desk cannot help you with an application issue you may contact the MCG application support team:",
+                        hometext6,
                         @"</span>",
                         
                         @"<p><ul><li>",
@@ -63,12 +125,12 @@
                         @"</li></ul></p>",
                         
                         @"<span>",
-                        @"Useful Websites:",
+                        hometext6a,
                         @"</span>",
                         
                         @"<p><ul><li>",
                         @"<span>",
-                        @"Click here to access the ATS Healthcare tracking website to track the delivery of your sample requests:",
+                        hometext7,
                         @"</span>",
                         @"</li></ul></p>",
                         
@@ -82,7 +144,7 @@
                         
                         @"<p><ul><li>",
                         @"<span>",
-                        @"Link To SampleCupboard:",
+                        hometext8,
                         @"</span>",
                         @"</li></ul></p>",
                         
@@ -107,6 +169,9 @@
 
 
 -(BOOL) webView:(UIWebView *)inWeb shouldStartLoadWithRequest:(NSURLRequest *)inRequest navigationType:(UIWebViewNavigationType)inType {
+    
+    // open links in safari
+    
     if ( inType == UIWebViewNavigationTypeLinkClicked ) {
         [[UIApplication sharedApplication] openURL:[inRequest URL]];
         return NO;
