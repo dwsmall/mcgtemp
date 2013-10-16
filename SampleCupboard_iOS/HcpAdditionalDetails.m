3e4fdc338f4e4ad89b816e1e1286c9f2c12aebfd
D.W.Small <dwsmall@gmail.com>
2013-10-16 15:06:29 -0400

    Update to language localization files

--- SampleCupboard_iOS/HcpAdditionalDetails.m
+++ SampleCupboard_iOS/HcpAdditionalDetails.m
@@ -10,6 +10,11 @@
 
 @interface HcpAdditionalDetails ()
 @property (strong, nonatomic) IBOutlet UINavigationBar *navBar;
+
+@property (strong, nonatomic) IBOutlet UINavigationItem *hcpTtileBar;
+
+
+
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 - (void)configureCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath;
 @end
@@ -21,10 +26,15 @@
 
 @synthesize currentHCPINFOEXT;
 
+#pragma mark - View Delegate
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    _hcpTtileBar.title = NSLocalizedString(@"HCP Additional Details", nil);
+    
 }
 
 
@@ -37,7 +47,7 @@
 
 
 
-#pragma mark - Table View
+#pragma mark - TableView Delegate
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     return 3;
@@ -57,11 +67,11 @@
             break;
             
         case 1:            
-            HeaderTitle = @"Details";
+            HeaderTitle = NSLocalizedString(@"Details",nil);
             break;
             
         case 2:            
-            HeaderTitle = @"Communication";
+            HeaderTitle = NSLocalizedString(@"Communication",nil);
             break;            
     }
     
@@ -140,38 +150,38 @@
     {
         case 0: //Address Type
             if (indexPath.row == 0) {
-                cell.textLabel.text = @"Address Type:";
+                cell.textLabel.text = NSLocalizedString(@"Address Type:",nil);
                 cell.detailTextLabel.text = currentHCPINFOEXT[8];
             }
             break;
             			
         case 1: //Details
             if (indexPath.row == 0) {
-                cell.textLabel.text = @"Facility Name:";
+                cell.textLabel.text = NSLocalizedString(@"Facility Name:",nil);
                 cell.detailTextLabel.text = currentHCPINFOEXT[9];
             }
             if (indexPath.row == 1) {
-                cell.textLabel.text = @"Address 1:";
+                cell.textLabel.text = NSLocalizedString(@"Address 1:",nil);
                 cell.detailTextLabel.text = currentHCPINFOEXT[10];
             }
             if (indexPath.row == 2) {
-                cell.textLabel.text = @"Address 2:";
+                cell.textLabel.text = NSLocalizedString(@"Address 2:",nil);
                 cell.detailTextLabel.text = currentHCPINFOEXT[11];
             }
             if (indexPath.row == 3) {
-                cell.textLabel.text = @"Address 3:";
+                cell.textLabel.text = NSLocalizedString(@"Address 3:",nil);
                 cell.detailTextLabel.text = currentHCPINFOEXT[12];
             }
             if (indexPath.row == 4) {
-                cell.textLabel.text = @"Province:";
+                cell.textLabel.text = NSLocalizedString(@"Province:",nil);
                 cell.detailTextLabel.text = currentHCPINFOEXT[13];
             }
             if (indexPath.row == 5) {
-                cell.textLabel.text = @"City:";
+                cell.textLabel.text = NSLocalizedString(@"City:",nil);
                 cell.detailTextLabel.text = currentHCPINFOEXT[14];
             }
             if (indexPath.row == 6) {
-                cell.textLabel.text = @"Postal:";
+                cell.textLabel.text = NSLocalizedString(@"Postal:",nil);
                 cell.detailTextLabel.text = currentHCPINFOEXT[15];
             }            
             break;
@@ -179,15 +189,15 @@
             
         case 2:  //Communication
             if (indexPath.row == 0) {
-                cell.textLabel.text = @"Telephone:";
+                cell.textLabel.text = NSLocalizedString(@"Telephone:",nil);
                 cell.detailTextLabel.text = currentHCPINFOEXT[5];
             }
             if (indexPath.row == 1) {
-                cell.textLabel.text = @"Extension:";
+                cell.textLabel.text = NSLocalizedString(@"Extension:",nil);
                 cell.detailTextLabel.text = currentHCPINFOEXT[16];
             }
             if (indexPath.row == 2) {
-                cell.textLabel.text = @"Fax:";
+                cell.textLabel.text = NSLocalizedString(@"Fax:",nil);
                 cell.detailTextLabel.text = currentHCPINFOEXT[6];
             }
             
